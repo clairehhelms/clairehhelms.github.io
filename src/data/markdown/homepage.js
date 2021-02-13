77c9165e8874e31bdef43781ede0cb40224e7fad
clairehhelms <clairehh@live.unc.edu>
2021-02-13 14:56:05 -0500

    changed text

--- src/data/markdown/homepage.js
+++ src/data/markdown/homepage.js
@@ -1,7 +1,5 @@
 const homepage = `
-My name is Claire, and I am in love. \n
-With what, you may ask? \n
-Browse my site to find out! \n
+My name is Claire. Thanks for visiting my website!
 `;
 
 export default homepage;
